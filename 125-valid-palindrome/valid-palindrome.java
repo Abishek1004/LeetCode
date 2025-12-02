0e945664f62bd43525f43df5870a05cc722371f4
Abishek <abishek10004@gmail.com>
2025-12-02 19:15:59 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (57.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,46 +1,46 @@
-class Solution {
-    public boolean isPalindrome(String s) {
+    class Solution {
+        public boolean isPalindrome(String s) {
 
-        int i = 0;
-        int j = s.length() - 1;
-        
-        while (i < j)
-        {
-            char left = s.charAt(i);
-            char right = s.charAt(j);
-
-            if (left >= 'A' && left <= 'Z')
+            int i = 0;
+            int j = s.length() - 1;
+            
+            while (i < j)
             {
-                left = (char) (left + 32);
+                char left = s.charAt(i);
+                char right = s.charAt(j);
+
+                if (left >= 'A' && left <= 'Z')
+                {
+                    left = (char) (left + 32);
+                }
+
+                if (right >= 'A' && right <= 'Z')
+                {
+                    right = (char) (right + 32);
+                }
+
+                if (!isAlphanumeric(left)) {
+                    i++;
+                } 
+
+                if (!isAlphanumeric(right)) {
+                    j--;
+                } 
+
+                if (isAlphanumeric(left) && isAlphanumeric(right)){
+                    if (left != right)
+                    return false;
+
+                    i++;
+                    j--;
+                }
             }
 
-            if (right >= 'A' && right <= 'Z')
-            {
-                right = (char) (right + 32);
-            }
-
-            if (!isAlphanumeric(left)) {
-                i++;
-            } 
-
-            if (!isAlphanumeric(right)) {
-                j--;
-            } 
-
-            if (isAlphanumeric(left) && isAlphanumeric(right)){
-                if (left != right)
-                return false;
-
-                i++;
-                j--;
-            }
+            return true;
         }
 
-        return true;
-    }
+        public boolean isAlphanumeric(char a) {
 
-    public boolean isAlphanumeric(char a) {
-
-        return (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9');
+            return (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9');
+        }
     }
-}
